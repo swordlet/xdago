0144f60dbc8b82ace7e8e48f2c6af1ddee897416
swordlet <tricorelzj@gmail.com>
2022-03-15 19:20:23 +0800

    kvstore interface

--- db/store.go
+++ db/store.go
@@ -1,1 +1,19 @@
 package db
+
+type FetchFunc func([]byte, []byte) bool
+
+type IKVSource interface {
+	GetName() string
+	SetName(name string)
+	IsAlive() bool
+	Init() error
+	Close()
+	Reset()
+	Put(K, V []byte) error
+	Get(K []byte) ([]byte, error)
+	Delete(K []byte) error
+	Keys() [][]byte
+	PrefixKeyLookup(key []byte) ([][]byte, error)
+	FetchPrefix(key []byte, f FetchFunc) error
+	PrefixValueLookup(key []byte) ([][]byte, error)
+}
