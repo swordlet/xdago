366af52ad752f4cce49b7d384a4629c95dc9dc13
swordlet <tricorelzj@gmail.com>
2022-03-18 15:06:41 +0800

    add log

--- db/kv.go
+++ db/kv.go
@@ -1,5 +1,19 @@
 package db
 
+type DatabaseName int
+
+const (
+	INDEX DatabaseName = iota // Block index
+
+	BLOCK // Block raw data.
+
+	TIME // Time related block.
+
+	ORPHANIND // Orphan block index
+
+	SNAPSHOT
+)
+
 type FetchFunc func([]byte, []byte) bool
 
 type IKVSource interface {
@@ -12,8 +26,13 @@ type IKVSource interface {
 	Put(K, V []byte) error
 	Get(K []byte) ([]byte, error)
 	Delete(K []byte) error
-	Keys() [][]byte
+	Keys() ([][]byte, error)
 	PrefixKeyLookup(key []byte) ([][]byte, error)
 	FetchPrefix(key []byte, f FetchFunc) error
 	PrefixValueLookup(key []byte) ([][]byte, error)
+}
+
+type IDataFactory interface {
+	GetDB(name DatabaseName) IKVSource
+	Close()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module xdago
 
 go 1.17
+
+require github.com/go-stack/stack v1.8.1
