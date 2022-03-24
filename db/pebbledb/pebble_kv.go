b977adba6c3d43b0af41c064c8c28e0bcab17408
swordlet <tricorelzj@gmail.com>
2022-03-24 14:22:07 +0800

    add pebble db implement for KV store

--- db/pebbledb/pebble_kv.go
+++ db/pebbledb/pebble_kv.go
@@ -2,13 +2,43 @@
 
 package pebbledb
 
-import "xdago/db"
+import (
+	"encoding/hex"
+	"errors"
+	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/bloom"
+	"os"
+	"path"
+	"sync"
+	"xdago/config"
+	"xdago/db"
+	"xdago/log"
+)
 
 type PebbleKv struct {
-	name  string
-	alive bool
+	sync.RWMutex
+	config           *config.Config
+	name             string
+	alive            bool
+	db               *pebble.DB
+	writeOpt         *pebble.WriteOptions
+	prefixSeekLength int
 }
 
+func (p *PebbleKv) SetConfig(config *config.Config) {
+	p.config = config
+}
+
+func NewPebbleKv(name string, preSeekLen int) *PebbleKv {
+	log.Debug("New db", log.Ctx{"db name": name})
+	return &PebbleKv{
+		name:             name,
+		prefixSeekLength: preSeekLen,
+		writeOpt: &pebble.WriteOptions{
+			Sync: true,
+		},
+	}
+}
 func (p *PebbleKv) GetName() string {
 	return p.name
 }
@@ -18,15 +48,100 @@ func (p *PebbleKv) SetName(name string) {
 }
 
 func (p *PebbleKv) Close() {
-
+	p.Lock()
+	defer p.Unlock()
+	if !p.alive {
+		return
+	}
+	log.Debug("Close db", log.Ctx{"db name": p.name})
+	err := p.db.Close()
+	if err != nil {
+		log.Crit("Failed close db", log.Ctx{"db name": p.name, "err": err})
+	}
+	p.alive = false
 }
 
 func (p *PebbleKv) Init() {
+	p.Lock()
+	defer p.Unlock()
 
+	log.Debug("~> PebbleKVSource.init()", log.Ctx{"db name": p.name})
+	if p.alive {
+		return
+	}
+	if p.name == "" {
+		log.Crit("no name set to db")
+	}
+
+	cache := pebble.NewCache(128 << 20)
+	defer cache.Unref()
+	opts := &pebble.Options{
+		Cache:                       cache,
+		Comparer:                    mvccComparer,
+		DisableWAL:                  false,
+		FormatMajorVersion:          pebble.FormatNewest,
+		L0CompactionThreshold:       2,
+		L0StopWritesThreshold:       1000,
+		LBaseMaxBytes:               64 << 20, // 64 MB
+		Levels:                      make([]pebble.LevelOptions, 7),
+		MaxConcurrentCompactions:    3,
+		MaxOpenFiles:                16384,
+		MemTableSize:                64 << 20,
+		MemTableStopWritesThreshold: 4,
+		Merger: &pebble.Merger{
+			Name: "cockroach_merge_operator",
+		},
+	}
+
+	for i := 0; i < len(opts.Levels); i++ {
+		l := &opts.Levels[i]
+		l.BlockSize = 32 << 10       // 32 KB
+		l.IndexBlockSize = 256 << 10 // 256 KB
+		l.FilterPolicy = bloom.FilterPolicy(10)
+		l.FilterType = pebble.TableFilter
+		if i > 0 {
+			l.TargetFileSize = opts.Levels[i-1].TargetFileSize * 2
+		}
+		l.EnsureDefaults()
+	}
+	opts.Levels[6].FilterPolicy = nil
+	opts.FlushSplitBytes = opts.Levels[0].TargetFileSize
+
+	opts.EnsureDefaults()
+
+	//if verbose {
+	//	opts.EventListener = pebble.MakeLoggingEventListener(nil)
+	//	opts.EventListener.TableDeleted = nil
+	//	opts.EventListener.TableIngested = nil
+	//	opts.EventListener.WALCreated = nil
+	//	opts.EventListener.WALDeleted = nil
+	//}
+	var err error
+	log.Info("Opening db", log.Ctx{"db name": p.name})
+	dbPath := p.getPath()
+	dir := path.Dir(dbPath)
+	if _, err = os.Stat(dir); os.IsNotExist(err) {
+		if err = os.Mkdir(dir, 0666); err != nil {
+			panic(err)
+		}
+	}
+	log.Debug("Open existing db or create new db ", log.Ctx{"db name": p.name})
+	p.db, err = pebble.Open(dir, opts)
+	if err != nil {
+		log.Crit("Failed to open db", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+	p.alive = true
+
+	log.Debug("<~ PebbleKVSource.init()", log.Ctx{"db name": p.name})
 }
 
 func (p *PebbleKv) Reset() {
-
+	p.Close()
+	err := os.RemoveAll(p.getPath())
+	if err != nil {
+		log.Crit("Failed to reset db", log.Ctx{"remove db": err.Error()})
+	}
+	p.Init()
 }
 
 func (p *PebbleKv) IsAlive() bool {
@@ -34,29 +149,144 @@ func (p *PebbleKv) IsAlive() bool {
 }
 
 func (p *PebbleKv) Put(key, val []byte) {
+	p.RLock()
+	defer p.Unlock()
 
+	log.Trace("~> PebbleKVSource.put():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key), "val": len(val)})
+	var err error
+	if val != nil {
+		if p.db == nil {
+
+		} else {
+			err = p.db.Set(key, val, p.writeOpt)
+		}
+	} else {
+		err = p.db.Delete(key, p.writeOpt)
+	}
+	if err != nil {
+		log.Crit("Failed to put into db", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+	log.Trace("<~ PebbleKVSource.put():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key), "val": len(val)})
 }
 
 func (p *PebbleKv) Get(key []byte) []byte {
-	return nil
+	p.RLock()
+	defer p.Unlock()
+
+	log.Trace("~> PebbleKVSource.get():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key)})
+
+	ret, closer, err := p.db.Get(key)
+	if closer != nil {
+		retCopy := make([]byte, len(ret))
+		copy(retCopy, ret)
+		ret = retCopy
+		closer.Close()
+	}
+	if errors.Is(err, pebble.ErrNotFound) {
+		return nil
+	}
+	if err != nil {
+		log.Crit("Failed to get from db", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+
+	log.Trace("<~ PebbleKVSource.get():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key), "val": len(ret)})
+
+	return ret
 }
 
 func (p *PebbleKv) Delete(key []byte) {
+	p.RLock()
+	defer p.Unlock()
 
+	log.Trace("~> PebbleKVSource.delete():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key)})
+	err := p.db.Delete(key, p.writeOpt)
+	if err != nil {
+		log.Crit("Failed to delete from db", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+
+	log.Trace("<~ PebbleKVSource.delete():",
+		log.Ctx{"db name": p.name, "key": hex.EncodeToString(key)})
 }
 
 func (p *PebbleKv) Keys() [][]byte {
-	return nil
-}
+	p.RLock()
+	defer p.Unlock()
 
-func (p *PebbleKv) PrefixKeyLookup(key []byte) [][]byte {
-	return nil
+	log.Trace("~> PebbleKVSource.keys():", log.Ctx{"db name": p.name})
+	var keys [][]byte
+	iter := p.db.NewIter(nil)
+	for iter.First(); iter.Valid(); iter.Next() {
+		keys = append(keys, iter.Key())
+	}
+	if err := iter.Close(); err != nil {
+		log.Crit("Failed to close iterator", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+
+	log.Trace("<~ PebbleKVSource.keys():", log.Ctx{"db name": p.name})
+	return keys
 }
 
 func (p *PebbleKv) FetchPrefix(key []byte, f db.FetchFunc) {
+	p.RLock()
+	defer p.Unlock()
 
+	iter := p.db.NewIter(prefixIterOptions(key))
+	for iter.First(); iter.Valid(); iter.Next() {
+		if f(iter.Key(), iter.Value()) {
+			break
+		}
+	}
+	if err := iter.Close(); err != nil {
+		log.Crit("Failed to close prefix iterator", log.Ctx{"db name": p.name, "err": err.Error()})
+	}
+}
+
+func (p *PebbleKv) PrefixKeyLookup(key []byte) [][]byte {
+	var keyList [][]byte
+	p.FetchPrefix(key, func(k, v []byte) bool {
+		keyList = append(keyList, k)
+		return false
+	})
+	return keyList
 }
 
 func (p *PebbleKv) PrefixValueLookup(key []byte) [][]byte {
-	return nil
+	var valueList [][]byte
+	p.FetchPrefix(key, func(k, v []byte) bool {
+		valueList = append(valueList, v)
+		return false
+	})
+	return valueList
+}
+
+func (p *PebbleKv) getPath() string {
+	return path.Join(p.config.StoreDir(), p.name)
+}
+
+func (p *PebbleKv) backupPath() string {
+	return path.Join(p.config.StoreDir(), "backup", p.name)
+}
+
+var keyUpperBound = func(b []byte) []byte {
+	end := make([]byte, len(b))
+	copy(end, b)
+	for i := len(end) - 1; i >= 0; i-- {
+		end[i] = end[i] + 1
+		if end[i] != 0 {
+			return end[:i+1]
+		}
+	}
+	return nil // no upper-bound
+}
+
+var prefixIterOptions = func(prefix []byte) *pebble.IterOptions {
+	return &pebble.IterOptions{
+		LowerBound: prefix,
+		UpperBound: keyUpperBound(prefix),
+	}
 }
--- go.mod
+++ go.mod
@@ -3,18 +3,31 @@ module xdago
 go 1.17
 
 require (
+	github.com/cockroachdb/pebble v0.0.0-20220322040433-6164579cf2cb
 	github.com/go-stack/stack v1.8.1
 	github.com/linxGnu/grocksdb v1.6.48
 	github.com/spf13/viper v1.10.1
 )
 
 require (
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.1.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
@@ -22,6 +35,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
